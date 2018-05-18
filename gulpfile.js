3cb22b1c65572640c656750323c811515265a24d
Martin Graham <martinjamesgraham@outlook.com>
2018-05-18 12:53:24 +0100

    attempt to package as node

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var clean = require('gulp-clean');
+var print = require('gulp-print');
+var mainNpmFiles = require('gulp-main-npm-files');
+var npmFiles = require("gulp-npm-files");
+var filter = require("gulp-filter");
+var serial = require("run-sequence");
+var bfy = require("gulp-browserify");
+
+var outFolder = 'dist';
+var outFolder2 = 'dist/vendor';
+var nm = 'node_modules';
+
+
+gulp.task('clean', function () {
+    return gulp.src(outFolder, {read: false})
+        .pipe(clean());
+});
+
+// Concat & Minify JS
+gulp.task('minify', function() {
+    return gulp.src([outFolder+'/js/*.js'])
+        .pipe(concat('d3table.js'))
+        .pipe(gulp.dest(outFolder+"/js"))
+        .pipe(rename('d3table.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(outFolder+"/js"));
+});
+
+// Concat & Minify CSS
+gulp.task('minifycss', function() {
+    return gulp.src([outFolder+'/css/*.css'])
+        .pipe(concat('userAdminDist.css'))
+        .pipe(gulp.dest(outFolder+"/css"))
+        .pipe(rename('userAdminDist.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest(outFolder+"/css"));
+});
+
+
+gulp.task('copy', [], function() {
+    return gulp.src (['*.html', './js/*', './css/*'], {base: '.'})
+        .pipe (print())
+        .pipe (gulp.dest(outFolder))
+    ;
+});
+
+gulp.task('bfy', [], function() {
+    return gulp.src (outFolder2+"/**/*.js", {base: '.'})
+        .pipe (print())
+        .pipe (bfy({
+            //insertGlobals : true,
+        }))
+        .pipe (gulp.dest("."))
+    ;
+});
+
+gulp.task ('copynpm', function () {
+    var mainjs = mainNpmFiles();
+    
+    console.log (mainjs);
+    
+    var delim = '/';
+    var rebasedMainjs = mainjs.map (function (js) {
+        js = js.replace ("/./", "/");
+        var split = js.split (delim);
+        return "**" + delim + split.slice(2).join(delim);
+    });
+    
+    var otherTypes = ['**/*.gif', '**/*.png', '**/*.jpg', '**/*.css', '!*/**/node_modules/**', '!**/docs/**', '!**/demos/**', '!**/examples/**', '!**/tests/**', '!**/jquery-ui/**/*.css', '!**/jquery-ui/**/*.js', '!**/zepto/**/*.js'];
+    var filterTo = rebasedMainjs.concat (otherTypes);
+    console.log ("f", filterTo);
+    
+    return gulp.src (npmFiles(), { base: "./"+nm })
+        .pipe (filter (filterTo))
+        .pipe (print())
+        .pipe (gulp.dest(outFolder2))
+    ;
+});
+
+
+gulp.task ('all', ['clean'], function () {
+    serial ('copy', 'copynpm');
+});
+
+// Default
+gulp.task('default', ['all']);
--- tests/tests.js
+++ tests/tests.js
@@ -1,97 +1,119 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
-});
-
 var table = demoSetup();
 
-QUnit.test("page size", function (assert) {
-  assert.deepEqual(table.pageSize(), 20, "Passed!" );
-  assert.deepEqual(table.getAllRowsSelection().size(), 20, "Passed!" );
+QUnit.test("Page size", function (assert) {
+	var expected = 20;
+  assert.deepEqual(table.pageSize(), expected, "Expected page size "+JSON.stringify(expected)+", Passed!");
+  assert.deepEqual(table.getAllRowsSelection().size(), expected, "Expected "+JSON.stringify(expected)+" rows in visible table, Passed!");
 });
 
-QUnit.test("data size", function (assert) {
-	assert.deepEqual(table.getData().length, 20000, "Passed!" );
-  assert.deepEqual(table.getData().length, table.getFilteredSize(), "Passed!" );
+QUnit.test("Unfiltered data size", function (assert) {
+	assert.deepEqual(table.getData().length, 20000, "Expected 20000 items in dataset, Passed!");
+  assert.deepEqual(table.getData().length, table.getFilteredSize(), "Unfiltered dataset and filtered dataset same size with no filter set, Passed!");
 });
 
-QUnit.test("filtered single id", function (assert) {
+QUnit.test("filtered single id and restore", function (assert) {
 	var filter = table.filter();
 	filter.id.value = "100";
 	table.filter(filter);
-  assert.deepEqual(table.getFilteredSize(), 1, "Passed!" );
+  assert.deepEqual(table.getFilteredSize(), 1, "Expected 1 item in filtered data set, Passed!");
+	
+	table.update();
+	var rowCount = table.getAllRowsSelection().size();
+  	assert.deepEqual (rowCount, 1, "1 row in visible table, Passed!");
+	
+	filter = table.filter();
+	filter.id.value = "";
+	table.filter(filter);
+  assert.deepEqual(table.getFilteredSize(), 20000, "Expected 20000 items in filtered data set on filter removal, Passed!");
+	
+	table.update();
+	rowCount = table.getAllRowsSelection().size();
+	assert.deepEqual(rowCount, 20, "20 rows in visible table, Passed!");
 });
 
-QUnit.test("filtered range id", function (assert) {
+QUnit.test("filtered range id and restore", function (assert) {
 	var filter = table.filter();
 	filter.id.value = "100 199";
 	table.filter(filter);
-  assert.deepEqual(table.getFilteredSize(), 100, "Passed!" );
-});
-
-QUnit.test("remove filter id", function (assert) {
-	var filter = table.filter();
+  assert.deepEqual(table.getFilteredSize(), 100, "Expected 100 items in filtered data set, Passed!");
+	
+	filter = table.filter();
 	filter.id.value = "";
 	table.filter(filter);
-  assert.deepEqual(table.getFilteredSize(), 20000, "Passed!" );
+  assert.deepEqual(table.getFilteredSize(), 20000, "Expected 20000 items in filtered data set on filter removal, Passed!");
 });
 
 QUnit.test("set filter object", function (assert) {
+	var expected = (20000/8);
 	var filter = table.filter();
 	filter.object.value = "cat";
 	table.filter(filter);
-  assert.deepEqual(table.getFilteredSize(), 20000/8, "Passed!" );
-	var filter = table.filter();
+  assert.deepEqual(table.getFilteredSize(), expected, "Expected "+expected+" items in filtered data set, Passed!");
+	
+	// restore table state
+	filter = table.filter();
 	filter.object.value = "";
 	table.filter(filter);
 });
 
 QUnit.test("set filter array", function (assert) {
+	var expected = (20000/5) + (20000/5) - (20000/25);
 	var filter = table.filter();
 	filter.array.value = "chorizo";
 	table.filter(filter);
-  assert.deepEqual(table.getFilteredSize(), (20000/5) + (20000/5) - (20000/25), "Passed!" );
-	var filter = table.filter();
+  assert.deepEqual(table.getFilteredSize(), expected, "Expected "+expected+" items in filtered data set, Passed!");
+	
+	// restore table state
+	filter = table.filter();
 	filter.id.value = "";
 	table.filter(filter);
 });
 
 QUnit.test("set conjunctive filter array and object", function (assert) {
+	var expected = ((20000/5) + (20000/5) - (20000/25)) / 8;
 	var filter = table.filter();
 	filter.array.value = "chorizo";
 	filter.object.value = "cat";
 	table.filter(filter);
-  assert.deepEqual(table.getFilteredSize(), ((20000/5) + (20000/5) - (20000/25)) / 8, "Passed!" );
-	var filter = table.filter();
+  assert.deepEqual(table.getFilteredSize(), expected, "Expected "+expected+" items in filtered data set, Passed!");
+	
+	// restore table state
+	filter = table.filter();
 	filter.array.value = "";
 	filter.object.value = "";
 	table.filter(filter);
 });
 
 QUnit.test("sort by array", function (assert) {
+	var expected = ["bratwurst", "bratwurst"];
 	table.orderKey("array").orderDir("asc").sort().update();
 	var firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
 	var firstRowData = firstRow.datum();
-  	assert.deepEqual (firstRowData.array, ["bratwurst", "bratwurst"], "Passed!" );
+  	assert.deepEqual (firstRowData.array, expected, "Expected "+JSON.stringify(expected)+" first in table, Passed!");
 	
+	expected = ["lorne", "lorne"];
 	table.orderDir("desc").sort().update();
-	var firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
-	var firstRowData = firstRow.datum();
-	assert.deepEqual (firstRowData.array, ["lorne", "lorne"], "Passed!" );
+	firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
+	firstRowData = firstRow.datum();
+	assert.deepEqual (firstRowData.array, expected, "Expected "+JSON.stringify(expected)+" first in table, Passed!");
 });
 
 QUnit.test("sort by id", function (assert) {
+	var expected = 1;
 	table.orderKey("id").orderDir("asc").sort().update();
 	var firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
 	var firstRowData = firstRow.datum();
-  	assert.deepEqual (firstRowData.id, 1, "Passed!" );
+  	assert.deepEqual (firstRowData.id, expected, "Expected id "+JSON.stringify(expected)+" first in table, Passed!");
 	
+	expected = 20000;
 	table.orderDir("desc").sort().update();
-	var firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
-	var firstRowData = firstRow.datum();
-	assert.deepEqual (firstRowData.id, 20000, "Passed!" );
+	firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
+	firstRowData = firstRow.datum();
+	assert.deepEqual (firstRowData.id, expected, "Expected id "+JSON.stringify(expected)+" first in table, Passed!");
 });
 
 QUnit.test("sort by id, then filter by array", function (assert) {
+	var expected = 2;
 	table.orderKey("id").orderDir("asc").sort();
 	var filter = table.filter();
 	filter.array.value = "chorizo";
@@ -99,41 +121,49 @@ QUnit.test("sort by id, then filter by array", function (assert) {
 	table.update();
 	var firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
 	var firstRowData = firstRow.datum();
-  	assert.deepEqual (firstRowData.id, 2, "Passed!" );
+  	assert.deepEqual (firstRowData.id, expected, "Expected id "+JSON.stringify(expected)+" first in table, Passed!");
 	
+	expected = 19997;
 	table.orderDir("desc").sort().update();
-	var firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
-	var firstRowData = firstRow.datum();
-	assert.deepEqual (firstRowData.id, 19997, "Passed!" );
+	firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
+	firstRowData = firstRow.datum();
+	assert.deepEqual (firstRowData.id, expected, "Expected id "+JSON.stringify(expected)+" first in table, Passed!");
 	
-	var filter = table.filter();
+	// restore table state
+	filter = table.filter();
 	filter.array.value = "";
 	table.filter(filter);
 });
 
 QUnit.test("filter by array, then sort by id", function (assert) {
+	var expected = 2;
 	var filter = table.filter();
 	filter.array.value = "chorizo";
 	table.filter(filter);
 	table.orderKey("id").orderDir("asc").sort().update();
 	var firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
 	var firstRowData = firstRow.datum();
-  	assert.deepEqual (firstRowData.id, 2, "Passed!" );
+  	assert.deepEqual (firstRowData.id, expected, "Expected id "+JSON.stringify(expected)+" first in table, Passed!");
 	
+	expected = 19997;
 	table.orderDir("desc").sort().update();
-	var firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
-	var firstRowData = firstRow.datum();
-	assert.deepEqual (firstRowData.id, 19997, "Passed!" );
+	firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
+	firstRowData = firstRow.datum();
+	assert.deepEqual (firstRowData.id, expected, "Expected id "+JSON.stringify(expected)+" first in table, Passed!");
 	
-	var filter = table.filter();
+	// restore table state
+	filter = table.filter();
 	filter.array.value = "";
-	table.filter(filter).orderDir("asc");
+	table.filter(filter).orderDir("asc").sort().update();
 });
 
 QUnit.test("page to 20", function (assert) {
+	var expected = 401;
 	table.page(21).update();
 	var firstRow = table.getAllRowsSelection().filter(function(d,i) { return i === 0; });
 	var firstRowData = firstRow.datum();
-  	assert.deepEqual (firstRowData.id, 401, "Passed!" );
+  	assert.deepEqual (firstRowData.id, expected, "Expected id "+JSON.stringify(expected)+" first in table, Passed!");
 });
+
+console.log ("table", table);
 
